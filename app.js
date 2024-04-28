867447d0c924f7cd5a7eba2813be5582283207d6
VincenzoCopia <vincenzo.copia@gmail.com>
2024-04-28 18:09:25 +0200

    add select month

--- app.js
+++ app.js
@@ -9,33 +9,45 @@ var Prod = database.ref("Produzione");
 var Anno = "2024";
 var Mese = "04";
 
-Prod.child("Oggi").on('value', function (sn) {
-    document.getElementById('Data').innerHTML = "" + sn.child("Data").val() + "";
-});
+// Prod.child("Oggi").on('value', function (sn) {
+//     document.getElementById('Data').innerHTML = "" + sn.child("Data").val() + "";
+// });
 
 var myData = [['Giorno', 'Tons', { role: 'style' }, { role: 'annotation' }]];
 
-Prod.child("Monitor").child(Anno).child(Mese).on('value', function (snapshot) {
-    myData = [['Giorno', 'Tons', { role: 'style' }, { role: 'annotation' }]];
-    var TotMese = 0;
-    var Conta = 0;
-
-    snapshot.forEach(function (cs) {
-        var Etichetta = cs.val() != 0 ? cs.val() : '';
-        myData.push([cs.key, Etichetta, 'blue', "" + Etichetta]);
-        if (cs.val() > 0) {
-            TotMese += cs.val();
-            Conta += 1;
-        }
-    });
-
-    document.getElementById('Media').innerHTML = Math.floor(TotMese / Conta);
-
-    google.charts.setOnLoadCallback(drawChart);
-});
+var Mese = "04";  // Mese predefinito
 
 google.charts.load('current', { 'packages': ['corechart'] });
-google.charts.setOnLoadCallback(drawChart);
+
+function updateMonth(selectedMonth) {
+
+    Mese = selectedMonth.split("-")[1];
+    Anno = selectedMonth.split("-")[0];
+    console.log(`Anno ${Anno} Mese ${Mese}`);
+    loadMonthlyData();  // Ricarica i dati per il nuovo mese
+}
+
+function loadMonthlyData() {
+    Prod.child("Monitor").child(Anno).child(Mese).on('value', function (snapshot) {
+        myData = [['Giorno', 'Tons', { role: 'style' }, { role: 'annotation' }]];
+        var TotMese = 0;
+        var Conta = 0;
+
+        snapshot.forEach(function (cs) {
+            var Etichetta = cs.val() != 0 ? cs.val() : '';
+            myData.push([cs.key, Etichetta, 'blue', "" + Etichetta]);
+            if (cs.val() > 0) {
+                TotMese += cs.val();
+                Conta += 1;
+            }
+        });
+
+        document.getElementById('Media').innerHTML = Math.floor(TotMese / Conta);
+        google.charts.setOnLoadCallback(drawChart);
+    });
+}
+
+google.charts.setOnLoadCallback(loadMonthlyData);  // Modifica qui per chiamare la nuova funzione all'avvio
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable(myData);
@@ -49,6 +61,42 @@ function drawChart() {
     var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
     chart.draw(data, options);
 }
+
+
+// Prod.child("Monitor").child(Anno).child(Mese).on('value', function (snapshot) {
+//     myData = [['Giorno', 'Tons', { role: 'style' }, { role: 'annotation' }]];
+//     var TotMese = 0;
+//     var Conta = 0;
+
+//     snapshot.forEach(function (cs) {
+//         var Etichetta = cs.val() != 0 ? cs.val() : '';
+//         myData.push([cs.key, Etichetta, 'blue', "" + Etichetta]);
+//         if (cs.val() > 0) {
+//             TotMese += cs.val();
+//             Conta += 1;
+//         }
+//     });
+
+//     document.getElementById('Media').innerHTML = Math.floor(TotMese / Conta);
+
+//     google.charts.setOnLoadCallback(drawChart);
+// });
+
+// google.charts.load('current', { 'packages': ['corechart'] });
+// google.charts.setOnLoadCallback(drawChart);
+
+// function drawChart() {
+//     var data = google.visualization.arrayToDataTable(myData);
+//     var options = {
+//         title: 'Produzione giornaliera',
+//         hAxis: { title: 'Giorno' },
+//         vAxis: { title: 'Tons', minValue: 0, maxValue: 1000 },
+//         legend: 'none'
+//     };
+
+//     var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+//     chart.draw(data, options);
+// }
 
 // Gestione della connessione
 var connectedRef = firebase.database().ref(".info/connected");
